a86feae50240bc84c8decf88e0340b9f19c84e09
DavidGhedini <david@davidghedini.com>
2016-05-05 11:34:05 +0200

    Admin Index

--- views/admin/index/index.php
+++ views/admin/index/index.php
@@ -1,32 +1,42 @@
 <?php
-
-echo head(array('title'=>'Escher', 'bodyclass'=>'escher browse'));
-
+echo head(array('title' => 'Escher', 'bodyclass' => 'escher browse'));
 ?>
+<style>
+    .escher-success{
+        background: #3c763d;
+        padding: 10px;
+        color: #fff;   
+        border: 1px solid transparent;
+        border-radius: 4px;
+    }
+    .escher-error{
+        background: #a94442;
+        padding: 10px;
+        color: #fff;  
+        border: 1px solid transparent;
+        border-radius: 4px;
+    }
+</style>
+<div class="mapfig-img"><img src="<?php echo url("escher"); ?>/../../plugins/Escher/image/escher-logo.gif" /></div>
 
-<div id='primary'>	  	
-    <p></p>
-</div>
+<?php
+if (isset($_GET['success']) && $_GET['success'] == 1) {
+    echo '<p class="escher-success">' . $_GET['msg'] . '</p>';
+} elseif (isset($_GET['error']) && $_GET['error'] == 1) {
+    echo '<p class="escher-error">' . $_GET['msg'] . '</p>';
+}
+?>
+<p>Select Plugin and Click Upload</p>
 
-<div class="mapfig-img"><img src="<?php echo  url("escher"); ?>/../../plugins/Escher/image/escher-logo.gif" /></div>
-
-Select Plugin and Click Upload
-
-<?php if (isset($_GET['s']) && $_GET['s'] == 1) : ?>
-	<p>Plugin uploaded successfully.</p>
-<?php endif ?>
-<?php if (isset($_GET['e']) && $_GET['e'] == 1) : ?>
-	<p>Plugin directory already exists.</p>
-<?php endif ?>
 <form action="<?php echo url("escher/index/upload"); ?>" method="post">
-	<p>
-		<select name="plugin-name">
-		<?php foreach($plugins as $v=>$k) { ?>
-			<option value="<?php echo $k; ?>"><?php echo $v; ?></option>
-		<?php } ?>
-		</select>
-	</p>
-	<input type="submit" value="Upload"/>
+    <p>
+        <select name="plugin-name">
+            <?php foreach ($plugins as $v => $k) { ?>
+                <option value="<?php echo $k; ?>"><?php echo $v; ?></option>
+            <?php } ?>
+        </select>
+    </p>
+    <input type="submit" value="Upload"/>
 </form>
 
 
